5f13bccada330b40924972dbc7b976ab0815949a
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-05-03 14:19:26 -0600

    adds capstone update action

--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -7,6 +7,18 @@ class CapstonesController < ApplicationController
   end
 
   def update
-    render json: {message: "in update"}
+    student = Student.find_by(id: params[:id])
+    capstone = Capstone.find_by(student_id: student.id)
+
+    capstone.name = params[:name] || capstone.name
+    capstone.description = params[:description] || capstone.description
+    capstone.url = params[:url] || capstone.url
+    capstone.screenshot = params[:screenshot] || capstone.screenshot
+
+    if capstone.save
+      render json: capstone.as_json 
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   patch "/capstones/:id" => "capstones#update"
 
   post "/sessions" => "sessions#create"
+  
 end
